0de87eb83428a23607de45467c6f27fb8f1f6713
Koketso Nkosi <koketsochrisnkosi@gmail.com>
2026-02-11 08:45:24 +0200

    Add music library data to data.js

---
+++ data.js
@@ -0,0 +1,126 @@
+// Music Library Data
+const musicLibrary = [
+    { 
+        id: 1, 
+        title: "Blinding Lights", 
+        artist: "The Weeknd", 
+        category: "pop", 
+        duration: "3:20", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" 
+    },
+    { 
+        id: 2, 
+        title: "Bohemian Rhapsody", 
+        artist: "Queen", 
+        category: "rock", 
+        duration: "5:55", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3" 
+    },
+    { 
+        id: 3, 
+        title: "Take Five", 
+        artist: "Dave Brubeck", 
+        category: "jazz", 
+        duration: "5:24", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3" 
+    },
+    { 
+        id: 4, 
+        title: "Strobe", 
+        artist: "Deadmau5", 
+        category: "electronic", 
+        duration: "10:36", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3" 
+    },
+    { 
+        id: 5, 
+        title: "Canon in D", 
+        artist: "Pachelbel", 
+        category: "classical", 
+        duration: "4:33", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3" 
+    },
+    { 
+        id: 6, 
+        title: "Shape of You", 
+        artist: "Ed Sheeran", 
+        category: "pop", 
+        duration: "3:53", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3" 
+    },
+    { 
+        id: 7, 
+        title: "Hotel California", 
+        artist: "Eagles", 
+        category: "rock", 
+        duration: "6:30", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3" 
+    },
+    { 
+        id: 8, 
+        title: "Blue Train", 
+        artist: "John Coltrane", 
+        category: "jazz", 
+        duration: "10:40", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3" 
+    },
+    { 
+        id: 9, 
+        title: "Levels", 
+        artist: "Avicii", 
+        category: "electronic", 
+        duration: "3:18", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3" 
+    },
+    { 
+        id: 10, 
+        title: "Spring", 
+        artist: "Vivaldi", 
+        category: "classical", 
+        duration: "10:03", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3" 
+    },
+    { 
+        id: 11, 
+        title: "Bad Guy", 
+        artist: "Billie Eilish", 
+        category: "pop", 
+        duration: "3:14", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" 
+    },
+    { 
+        id: 12, 
+        title: "Stairway to Heaven", 
+        artist: "Led Zeppelin", 
+        category: "rock", 
+        duration: "8:02", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3" 
+    },
+    { 
+        id: 13, 
+        title: "So What", 
+        artist: "Miles Davis", 
+        category: "jazz", 
+        duration: "9:22", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3" 
+    },
+    { 
+        id: 14, 
+        title: "Sandstorm", 
+        artist: "Darude", 
+        category: "electronic", 
+        duration: "3:48", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3" 
+    },
+    { 
+        id: 15, 
+        title: "Moonlight Sonata", 
+        artist: "Beethoven", 
+        category: "classical", 
+        duration: "14:42", 
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3" 
+    }
+];
+
+// Export for use in other files
+window.musicLibrary = musicLibrary;
